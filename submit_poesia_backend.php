9c85434e47fb7a2e60f617b30f626644f83bef82
chiaramgl <96255714+chiaramgl@users.noreply.github.com>
2025-01-04 14:40:11 +0100

    Update submit_poesia_backend.php

--- submit_poesia_backend.php
+++ submit_poesia_backend.php
@@ -1,17 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 $method = $_SERVER['REQUEST_METHOD'];
+$file_path = 'poesie.json';
 
 if ($method == 'POST') {
     // Recupera i dati inviati dal form
     $nome = htmlspecialchars($_POST['nome']);
     $poesia = htmlspecialchars($_POST['poesia']);
-
-    // Path del file dove salvare le poesie
-    $file_path = 'poesie.json';
 
     // Crea il file se non esiste
     if (!file_exists($file_path)) {
@@ -27,10 +25,9 @@ if ($method == 'POST') {
     // Salva tutte le poesie
     file_put_contents($file_path, json_encode($poesie));
 
-    echo "Poesia inviata con successo!";
+    echo json_encode(['message' => 'Poesia inviata con successo!']);
 } elseif ($method == 'GET') {
     // Recupera tutte le poesie
-    $file_path = 'poesie.json';
     if (file_exists($file_path)) {
         $poesie = file_get_contents($file_path);
         echo $poesie;
@@ -39,6 +36,6 @@ if ($method == 'POST') {
     }
 } else {
     http_response_code(403);
-    echo "Accesso vietato.";
+    echo json_encode(['message' => 'Accesso vietato.']);
 }
 ?>
